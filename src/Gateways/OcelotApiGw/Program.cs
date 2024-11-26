54fe6ed3e12d234abc9617ef8a66749fc219e670
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-11-26 10:51:51 +0100

    gateway implementation

---
+++ src/Gateways/OcelotApiGw/Program.cs
@@ -0,0 +1,38 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var environment = builder.Environment.EnvironmentName;
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+                      .AddJsonFile($"ocelot.{environment}.json", optional: true, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseOcelot().Wait();
+
+app.Run();
