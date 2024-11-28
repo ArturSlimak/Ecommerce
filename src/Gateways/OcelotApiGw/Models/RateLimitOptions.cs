21b22b709028244040af2412c867588fac5019d9
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-11-28 08:46:51 +0100

    Rate limiting

---
+++ src/Gateways/OcelotApiGw/Models/RateLimitOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace OcelotApiGw.Models;
+
+public class RateLimitOptions
+{
+    public const string RateLimit = "RateLimit";
+    public TokenBucketOptions TokenBucket { get; set; } = new();
+}
+
+public class TokenBucketOptions
+{
+    public string SectionName => "TokenBucket";
+    public string PolicyName { get; set; } = null!;
+    public int TokenLimit { get; set; }
+    public int QueueLimit { get; set; }
+    public int ReplenishmentPeriod { get; set; }
+    public int TokensPerPeriod { get; set; }
+    public bool AutoReplenishment { get; set; }
+}
+
--- src/Gateways/OcelotApiGw/Program.cs
+++ src/Gateways/OcelotApiGw/Program.cs
@@ -11,6 +11,26 @@ builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange
 builder.Services.AddOcelot(builder.Configuration);
 
 
+// Rate Limit  - commented since there is unresloved conflict with new rate limiting in .NET 8 and the one from Ocelot
+/*var rateLimitOptions = new RateLimitOptions();
+builder.Configuration.GetSection(RateLimitOptions.RateLimit).Bind(rateLimitOptions);
+builder.Services.AddRateLimiter(options =>
+{
+    var tokenBucketOptions = rateLimitOptions.TokenBucket;
+
+    options.AddTokenBucketLimiter(policyName: tokenBucketOptions.PolicyName, limiterOptions =>
+    {
+        limiterOptions.TokenLimit = tokenBucketOptions.TokenLimit;
+        limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        limiterOptions.QueueLimit = tokenBucketOptions.QueueLimit;
+        limiterOptions.ReplenishmentPeriod = TimeSpan.FromSeconds(tokenBucketOptions.ReplenishmentPeriod);
+        limiterOptions.TokensPerPeriod = tokenBucketOptions.TokensPerPeriod;
+        limiterOptions.AutoReplenishment = tokenBucketOptions.AutoReplenishment;
+    });
+    options.RejectionStatusCode = 429;
+});*/
+
+
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -26,6 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+//app.UseRateLimiter();
 
 app.UseHttpsRedirection();
 
