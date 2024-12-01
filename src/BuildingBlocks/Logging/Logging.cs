bc2664b6cb17e8653c5502af416ca93b7862d9b6
ArturSlimac <artur.nenakhov@student.hogent.be>
2024-12-01 09:34:17 +0100

    docker elastic + kibana startup

--- src/BuildingBlocks/Logging/Logging.cs
+++ src/BuildingBlocks/Logging/Logging.cs
@@ -1,4 +1,6 @@
 ﻿
+using Elastic.Serilog.Sinks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
@@ -6,15 +8,21 @@ namespace Logging;
 
 public static class Logging
 {
+    private static readonly string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [{ServiceName}/{Environment}] {Message:lj}{NewLine}{Exception}";
     public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
       (context, configuration) =>
       {
+          var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
           configuration
               .Enrich.FromLogContext()
               .Enrich.WithProperty("ContentRootPath", context.HostingEnvironment.ContentRootPath)
               .Enrich.WithProperty("ServiceName", context.Configuration["Serilog:Properties:ServiceName"] ?? "DefaultService")
               .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
-              .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [{ServiceName}/{Environment}] {Message:lj}{NewLine}{Exception}")
+              .WriteTo.Console(outputTemplate: outputTemplate)
+              .WriteTo.Elasticsearch(new[] { new Uri(elasticUri) }, opts =>
+              {
+
+              })
               .ReadFrom.Configuration(context.Configuration);
       };
 }
